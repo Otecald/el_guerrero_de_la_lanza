b63bf9a56c9655fd5b732b8146d9e33c5ea63a3b
Emiliano Ezequiel Parenti <passymas@gmail.com>
2020-03-20 20:13:05 -0300

    Add files via upload

---
+++ utilidades.js
@@ -0,0 +1,22 @@
+function dibujar(){
+	holamundo(lienzo,[color.value,bckgrnd.value,lienzo.width,texto.value])
+}
+function fC(){
+	return document.getElementsByTagName("canvas")[0]
+}
+function init()
+{
+	// WorldTimer = null
+	SpriteList = Array();
+	Camera = {
+		position: {
+			x: 0,
+			y: 0
+		},
+		width: fC().width,
+		height: fC().height
+	}
+	createWorld(fC())
+}
+document.body.onload = onload=init
+
