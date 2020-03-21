527f71f86bb33036622cec0cdab77abf7347b362
ArtEze <passymas@gmail.com>
2020-03-21 14:51:15 -0300

    Descarga de Bitbucket. Cambiando rutas mediante expresiones regulares.

--- bitbucket/programa.js
+++ bitbucket/programa.js
@@ -1,26 +1,61 @@
+function descargar(url,es_json,callback){
+	var tipo = es_json?"json":"text"
+	return fetch(url).then(x=>x[tipo]()).then(callback)
+}
 function descargar_bitbucket(usuario,proyecto,ruta,callback){
 	var url_api = "https://api.bitbucket.org/2.0/repositories/"
-	return (fetch(url_api+usuario+"/"+proyecto+"/src/")
-		.then(x=>x.json())
-		.then(x=>{
-			var regex=new RegExp("/"+ruta+"$","")
-			var url = (x.values
-				.map(x=>x.links.self.href)
-				.filter(x=>x.match(regex))[0]
-			)
-			return callback(url)
-		})
+	var url = url_api+usuario+"/"+proyecto+"/src/"
+	return descargar(url,true,x=>{
+		var regex = new RegExp("/"+ruta+"$","")
+		var url_archivo = (x.values
+			.map(x=>x.links.self.href)
+			.filter(x=>x.match(regex))[0]
+		)
+		return callback(url_archivo)
+	})
+}
+function procesar_texto(texto,url){
+	var regex = new RegExp("https://dl.dropboxusercontent.com/u/3906848/Proyecto%20matiz/","g")
+	var regex_2 = new RegExp("(img.src = )([\"\']?)(.*)([\"\']?)","g")
+	return (texto
+		.replace(regex,"")
+		.replace(regex_2,'$1'+'"' + url + '" + '+'$2$3$4')
 	)
 }
-function insertar_html(url){
-	var descarga = (fetch(url)
-		.then(x=>x.text())
-		.then(x=>{
-			var y = url.split("/").slice(0,-1).concat("").join("/")
-			var z = x.replace(/(href|src)(\s*=)(\s*["']?)(\S+)/g,'$1$2$3'+y+'$4')
-			document.querySelector("html").innerHTML=z
-		})
+function descargar_scripts(lista,callback){
+	var existentes = (
+		Array.from(
+			new Set(
+				Array.from(document.querySelectorAll("script"))
+			)
+		)
+		.concat()
 	)
+	var descargas = lista.map(x=>{
+		descargar(x,false,callback)
+	})
+	var locales = existentes.filter(x=>!x.src).map(x=>{
+		var texto = x.innerHTML
+		callback(texto)
+	})
+	return locales
+}
+function insertar_html(url){
+	return descargar(url,false,x=>{
+		var url_proyecto_bitbucket = url.split("/").slice(0,-1).concat("").join("/")
+		var scripts = x.match(/src=".+"/g).map(x=>url_proyecto_bitbucket+x.split("=")[1].slice(1,-1))
+		var texto_modificado = (x
+			.replace(/(href)(\s*=)(\s*["']?)(\S+)/g,'$1$2$3'+url_proyecto_bitbucket+'$4')
+			.replace(/<script.*>.*<\/script>\s*/g,"")
+		)
+		document.querySelector("html").innerHTML=texto_modificado
+		function modificar_e_insertar_script(texto,procesar){
+			var script = document.createElement("script")
+			script.innerHTML = procesar_texto(texto,url_proyecto_bitbucket)
+			document.head.appendChild(script)
+		}
+		var descargas = descargar_scripts(scripts,modificar_e_insertar_script)
+	})
 }
 function descargar_el_guerrero_de_la_lanza_desde_bitbucket(){
 	return descargar_bitbucket("ArtEze","el_guerrero_de_la_lanza","index.html",insertar_html)
